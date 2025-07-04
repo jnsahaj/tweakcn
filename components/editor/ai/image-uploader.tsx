73b102f172cd654baf8148b6f18dc042d0c516ee
Luis Llanes <luisllaboj@gmail.com>
2025-07-04 02:49:32 -0700

    feat: AI components now support and display images

--- components/editor/ai/image-uploader.tsx
+++ components/editor/ai/image-uploader.tsx
@@ -4,20 +4,45 @@ import { cn } from "@/lib/utils";
 import { ImagePlus } from "lucide-react";
 import { ComponentProps } from "react";
 
-interface ImageUploaderProps extends ComponentProps<typeof Button> {}
+interface ImageUploaderProps extends ComponentProps<typeof Button> {
+  fileInputRef: React.RefObject<HTMLInputElement | null>;
+  handleImageSelect: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  aiGenerateLoading: boolean;
+}
 
-export function ImageUploader({ disabled, className, ...props }: ImageUploaderProps) {
+export function ImageUploader({
+  fileInputRef,
+  handleImageSelect,
+  disabled,
+  className,
+  ...props
+}: ImageUploaderProps) {
   return (
-    <TooltipWrapper label="Attach image" asChild>
-      <Button
-        variant="outline"
-        size="sm"
-        className={cn("flex items-center gap-1.5 shadow-none", "@max-[350px]/form:w-8", className)}
+    <>
+      <input
+        type="file"
+        accept="image/*"
+        className="hidden"
+        aria-label="Upload image for theme generation"
+        ref={fileInputRef}
+        onChange={handleImageSelect}
         disabled={disabled}
-        {...props}
-      >
-        <ImagePlus /> <span className="hidden @[350px]/form:inline-flex">Image</span>
-      </Button>
-    </TooltipWrapper>
+      />
+      <TooltipWrapper label="Attach image" asChild>
+        <Button
+          variant="outline"
+          size="sm"
+          className={cn(
+            "flex items-center gap-1.5 shadow-none",
+            "@max-[350px]/form:w-8",
+            className
+          )}
+          disabled={disabled}
+          {...props}
+        >
+          <ImagePlus /> <span className="hidden @[350px]/form:inline-flex">Image</span>
+        </Button>
+      </TooltipWrapper>
+    </>
   );
 }
