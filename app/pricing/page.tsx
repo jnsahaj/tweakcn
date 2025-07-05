7c43399eb59c7af5ed07e5b454da558ffb9563c4
Sahaj Jain <sahajjain13@gmail.com>
2025-07-06 02:44:26 +0530

    fix: pricing navigation

--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import { useScramble } from "use-scramble";
 
 export default function PricingPage() {
