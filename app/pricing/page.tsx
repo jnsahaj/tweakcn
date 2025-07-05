8a1a24e94078bcf8cff4529293aaaacf19458483
Sahaj Jain <sahajjain13@gmail.com>
2025-07-06 03:11:33 +0530

    feat: Checkout link generation

--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -1,5 +1,9 @@
 "use client";
+
+import { createCheckout } from "@/actions/checkout";
+import { toast } from "@/hooks/use-toast";
 import { useRouter } from "next/navigation";
+import { useTransition } from "react";
 import { useScramble } from "use-scramble";
 
 export default function PricingPage() {
@@ -34,16 +38,29 @@ export default function PricingPage() {
     chance: 0.8,
   });
 
+  const [isPending, startTransition] = useTransition();
+
+  const handleOpenCheckout = async () => {
+    startTransition(async () => {
+      const res = await createCheckout();
+      if ("error" in res || !res.url) {
+        toast({
+          title: "Error",
+          description: res.error || "Failed to create checkout",
+          variant: "destructive",
+        });
+        return;
+      }
+
+      router.push(res.url);
+    });
+  };
+
   return (
     <div className="flex flex-grow flex-col items-center justify-center">
       <h1 className="text-9xl font-bold" ref={ราคาRef} onMouseOver={ราคาReplay} />
-      <button
-        onClick={() => {
-          router.push(process.env.NEXT_PUBLIC_CHECKOUT_LINK!);
-        }}
-      >
-        Open Checkout
-      </button>
+      <button onClick={handleOpenCheckout}>Open Checkout</button>
+      {isPending && <p>Loading...</p>}
       <p
         className="text-muted-foreground mt-24 text-2xl font-semibold"
         ref={essentialsRef}
