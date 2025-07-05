3561470636938018b05dfd4bd130ad1d951260cd
Sahaj Jain <sahajjain13@gmail.com>
2025-07-05 21:37:15 +0530

    add checkout link support

--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -1,7 +1,9 @@
 "use client";
+import { useRouter } from "next/router";
 import { useScramble } from "use-scramble";
 
 export default function PricingPage() {
+  const router = useRouter();
   const { ref: ราคาRef, replay: ราคาReplay } = useScramble({
     text: "$0",
     speed: 1,
@@ -35,6 +37,13 @@ export default function PricingPage() {
   return (
     <div className="flex flex-grow flex-col items-center justify-center">
       <h1 className="text-9xl font-bold" ref={ราคาRef} onMouseOver={ราคาReplay} />
+      <button
+        onClick={() => {
+          router.push(process.env.NEXT_PUBLIC_CHECKOUT_LINK!);
+        }}
+      >
+        Open Checkout
+      </button>
       <p
         className="text-muted-foreground mt-24 text-2xl font-semibold"
         ref={essentialsRef}
--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,7 +1,6 @@
 "use client";
 
 import { createAuthClient } from "better-auth/react";
-import { polar } from "./polar";
 
 export const authClient = createAuthClient({
   baseURL: process.env.BASE_URL,
