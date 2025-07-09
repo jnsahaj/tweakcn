9e840e82d56ba940a4563b588b5226968ce34b1d
Luis Llanes <luisllaboj@gmail.com>
2025-07-08 17:22:38 -0700

    chore: refactor components and layout/pages for consistency

--- app/settings/layout.tsx
+++ app/settings/layout.tsx
@@ -1,21 +1,25 @@
-import Logo from "@/assets/logo.svg";
+import { Header } from "@/components/header";
+import { Button } from "@/components/ui/button";
+import { ArrowLeft } from "lucide-react";
+import Link from "next/link";
 import { SettingsSidebar } from "./components/settings-sidebar";
-import { UserInfo } from "./components/user-info";
 
 export default async function SettingsLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex flex-col">
-      {/* Header section with logo and user info */}
-      <header className="flex h-24 items-center">
-        <div className="mx-auto flex w-full max-w-7xl items-center space-x-4 px-4 md:px-6">
-          <Logo className="size-8" />
-          <UserInfo />
-        </div>
-      </header>
+      <Header />
 
-      <main className="mx-auto flex w-full max-w-7xl flex-1 flex-col gap-4 px-4 py-16 md:flex-row md:px-6">
+      <div className="container mx-auto flex w-full max-w-7xl items-center justify-between px-4 pt-8 pb-4 md:px-6">
+        <Button variant="ghost" asChild>
+          <Link href="/editor/theme">
+            <ArrowLeft /> Back to Editor
+          </Link>
+        </Button>
+      </div>
+
+      <main className="container mx-auto flex w-full max-w-7xl flex-1 flex-col gap-4 px-4 py-4 md:flex-row md:px-6 md:py-8">
         <SettingsSidebar />
-        <div className="flex-1">{children}</div>
+        <div className="mx-auto w-full max-w-4xl flex-1">{children}</div>
       </main>
     </div>
   );
