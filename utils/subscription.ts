902a8803b37a2dc86d91b81605aa6a5bcad12996
Sahaj Jain <sahajjain13@gmail.com>
2025-07-16 01:07:16 +0530

    feat: Add faq to pricing page

--- utils/subscription.ts
+++ utils/subscription.ts
@@ -1,3 +1,5 @@
+import { AI_REQUEST_FREE_TIER_LIMIT, MAX_FREE_THEMES } from "@/lib/constants";
+
 type Feature = {
   description: string;
   status: "done" | "pending";
@@ -5,8 +7,8 @@ type Feature = {
 
 export const FREE_SUB_FEATURES: Feature[] = [
   { description: "Full theme customization", status: "done" },
-  { description: "5 AI generated themes", status: "done" },
-  { description: "Save and share up to 10 themes", status: "done" },
+  { description: `${AI_REQUEST_FREE_TIER_LIMIT} AI generated themes`, status: "done" },
+  { description: `Save and share up to ${MAX_FREE_THEMES} themes`, status: "done" },
   { description: "Import theme using CSS variables", status: "done" },
   { description: "Export theme via CSS variables", status: "done" },
   { description: "Export theme via Shadcn Registry Command", status: "done" },
@@ -16,6 +18,7 @@ export const FREE_SUB_FEATURES: Feature[] = [
 export const PRO_SUB_FEATURES: Feature[] = [
   { description: "Save and share unlimited themes", status: "done" },
   { description: "Unlimited AI generated themes", status: "done" },
+  { description: "Generate themes from images using AI", status: "done" },
   { description: "Generate beautiful themes from images", status: "done" },
   { description: "Priority support", status: "done" },
   { description: "Save your own fonts and colors", status: "pending" },
