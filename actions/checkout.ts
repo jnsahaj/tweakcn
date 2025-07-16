76c27d67653231eeb1c938e0fcf40e6a3208cf65
Sahaj Jain <sahajjain13@gmail.com>
2025-07-16 21:43:37 +0530

    feat: Create customer before creating checkout for them

--- actions/checkout.ts
+++ actions/checkout.ts
@@ -2,15 +2,16 @@
 
 import { polar } from "@/lib/polar";
 import { getCurrentUser, logError } from "@/lib/shared";
+import { getOrCreateCustomer } from "./customer";
 
 export const createCheckout = async () => {
   try {
     const user = await getCurrentUser();
+    const customer = await getOrCreateCustomer(user);
     const checkout = await polar.checkouts.create({
       products: [process.env.NEXT_PUBLIC_TWEAKCN_PRO_PRODUCT_ID!],
-      externalCustomerId: user.id,
+      customerId: customer?.id,
       successUrl: `${process.env.BASE_URL}/success?checkout_id={CHECKOUT_ID}`,
-      customerEmail: user.email,
     });
 
     return { url: checkout.url };
